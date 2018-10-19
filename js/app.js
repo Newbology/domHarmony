7afc8b91a4865fb2656005b62c69305453aa6797
kevin tauzer <kevintauzer97@gmail.com>
2018-10-19 11:33:00 -1000

    problems 1-7

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
+matCount.innerHTML = 11;
 
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+msgCount.innerHTML = 23;
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
+fullname.innerHTML = "Ronald Mcdonald";
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+age.innerHTML = 63;
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
+var jobTitle = document.createElement('div');
+jobTitle.id='job';
+jobTitle.innerHTML = "Clown and Restauranteur";
+data.appendChild(jobTitle);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
+var aboutMe = document.createElement('div');
+aboutMe.id='hobbies';
+aboutMe.innerHTML= "Long romantic walks on the beach, candle light big mac dinners and tormenting the king."
+data.appendChild(jobTitle);
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
+var locBox = document.createElement('div');
+locBox.id="location";
+locBox.innerHTML = 'Honolulu, HI';
+data.appendChild(locBox);
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
+
+
+
 
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
 
